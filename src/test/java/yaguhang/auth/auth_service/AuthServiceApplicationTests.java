3e3ac3ba7da36eaa475ba339c14865a446fa9375
m0304s <cms8955@naver.com>
2025-05-16 22:04:27 +0900

    Initial Commit

---
+++ src/test/java/yaguhang/auth/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yaguhang.auth.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
